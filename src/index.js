04bd8f96ec2dd66871dd7337ca809a79c81c20b5
cpbram2011 <cpbram2011@gmail.com>
2021-04-16 07:51:30 -0500

    fix auth timeout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+
 import configureStore from './store/store';
 
 import jwt_decode from 'jwt-decode';
@@ -12,18 +13,21 @@ import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
-  
+
   if (localStorage.jwtToken) {
 
     setAuthToken(localStorage.jwtToken);
+
     const decodedUser = jwt_decode(localStorage.jwtToken);
+
     const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
     store = configureStore(preloadedState);
-    // const currentTime = Date.now() / 1000;
-    window.exp = decodedUser.exp;
-    debugger
-    // if (decodedUser.exp < currentTime) {
-    if (decodedUser.exp * 1000 - Date.now() < 0) {
+
+    const currentTime = Date.now();
+    
+    if (decodedUser.exp * 1000 < currentTime) {
+
       debugger
       store.dispatch(logout());
       window.location.href = '/login';
