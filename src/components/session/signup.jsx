d5490c198ccae1994b954cfd2c6b055c7063b1d0
cpbram2011 <cpbram2011@gmail.com>
2021-05-14 22:14:00 -0500

    minor style tweaks

--- src/components/session/signup.jsx
+++ src/components/session/signup.jsx
@@ -8,9 +8,13 @@ const SignupForm = () => {
     const errors = useSelector(state => state.errors.session);
     window.errors = errors;
     
-    const useInput = type => {
+    const useInput = (type, prefill) => {
         const [value, setValue] = useState("");
-        const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
+        const input = <input 
+        value={value} 
+        onChange={e => setValue(e.target.value)} 
+        placeholder={prefill}
+        type={type} />;
         return [value, input];
     }
     
@@ -18,10 +22,10 @@ const SignupForm = () => {
         <p className='error'>{msg}</p>
     )
 
-    const [username, usernameInput] = useInput("Text")
-    const [email, emailInput] = useInput("Text")
-    const [password, passwordInput] = useInput("Password")
-    const [password2, password2Input] = useInput("Password")
+    const [username, usernameInput] = useInput("Text", "Username")
+    const [email, emailInput] = useInput("Text", "Email")
+    const [password, passwordInput] = useInput("Password", "Password")
+    const [password2, password2Input] = useInput("Password", "Re-type password")
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -34,25 +38,25 @@ const SignupForm = () => {
         <>
         <form onSubmit={handleSubmit}>
             <label  className={'username' in errors ? 'red-border' : null}>
-                Username
+                
                 {usernameInput}
             </label>
             {'username' in errors ? formatErr(errors.username) : null}
             <label  className={'email' in errors ? 'red-border' : null}>
-                Email
+                
                 {emailInput}
             </label>
             {'email' in errors ? formatErr(errors.email) : null}
             <label className={'password' in errors ? 'red-border' : null}>
-                Password
                 {passwordInput}
             </label>
             {'password' in errors ? formatErr(errors.password) : null}
             <label className={'password2' in errors ? 'red-border' : null}>
-                Re-type Password
+                
                 {password2Input}
             </label>
             {'password2' in errors ? formatErr(errors.password2) : null}
+            <br /><br />
             <input type="submit"/>
         </form>
         </>
